7b14106b8618cf0b92fd9de3dc5c7880cbd4c1ac
Iordache Paul-Tiberiu <123265734+PaulTiberiu@users.noreply.github.com>
2023-11-23 20:07:32 +0100

    Update TBox_ABox.pl

--- TBox_ABox.pl
+++ TBox_ABox.pl
@@ -1,20 +1,27 @@
 /* Concepts atomiques, roles et instances*/
 
+/*Predicats pour les concepts atomiques*/
 cnamea(personne).
 cnamea(livre).
 cnamea(objet).
 cnamea(sculpture).
 cnamea(anything).
 cnamea(nothing).
+
+/*Predicats pour les concepts non atomiques*/
 cnamena(auteur).
 cnamena(editeur).
 cnamena(sculpteur).
 cnamena(parent).
+
+/*Predicats pour les instances*/
 iname(michelAnge).
 iname(david).
 iname(sonnets).
 iname(vinci).
 iname(joconde).
+
+/*Predicats pour les roles*/
 rname(aCree).
 rname(aEcrit).
 rname(aEdite).
@@ -29,10 +36,13 @@ equiv(parent,and(personne,some(aEnfant,anything))).
 
 /*ABox*/
 
+/*Predicats pour les instantiations de concepts*/
 inst(michelAnge,personne).
 inst(david,sculpture).
 inst(sonnets,livre).
 inst(vinci,personne).
+
+/*Predicats pour les instantiations de roles*/
 inst(joconde,objet).
 instR(michelAnge, david, aCree).
 instR(michelAnge, sonnets, aEcrit).
